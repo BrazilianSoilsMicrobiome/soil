5ba72e9ec9dc8be96812338f838c58bedfb95b62
BrazilianSoilsMicrobiome <daniel.morais@brmicrobiome.org>
2021-10-23 22:27:52 -0200

    add script to modify html file

---
+++ modify_html_page_to_add_layout.R
@@ -0,0 +1,16 @@
+
+#setwd(dir = "G:/My Drive/BrazilianSoilsMicrobiome/github_page/soil/")
+
+rmarkdown::render(input = "index.Rmd", output_format = "html_document",
+                  output_file = "index.html")
+
+new_page <- read.delim(file = "index.html", header = F)
+row.names(new_page) <- 4:(nrow(new_page)+3)
+
+layout_lines <- data.frame(col1 = c("---", "layout = default", "---"))
+page_with_layout <- c(layout_lines[,1], new_page[,1])
+
+
+write.table(x = page_with_layout, file = "index.html",
+            quote = F,
+            row.names = F)
