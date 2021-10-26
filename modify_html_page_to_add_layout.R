5f117a662933135119b4ac8c752b895d0ba3f5a1
BrazilianSoilsMicrobiome <daniel.morais@brmicrobiome.org>
2021-10-26 00:36:48 -0200

    using html editing by hand

--- modify_html_page_to_add_layout.R
+++ modify_html_page_to_add_layout.R
@@ -1,6 +1,6 @@
 
 #setwd(dir = "G:/My Drive/BrazilianSoilsMicrobiome/github_page/soil/")
-
+# broken :///
 rmarkdown::render(input = "index.Rmd", output_format = "html_document",
                   output_file = "index.html")
 
@@ -8,7 +8,7 @@ new_page <- read.delim(file = "index.html", header = F)
 row.names(new_page) <- 4:(nrow(new_page)+3)
 
 layout_lines <- data.frame(col1 = c("---", "layout: default", "---"))
-page_with_layout <- c(layout_lines[,1], new_page[,1])
+page_with_layout <- c(layout_lines[,1], new_page[c(2 : nrow(new_page)),1])
 
 
 write.table(x = page_with_layout, file = "index.html",
