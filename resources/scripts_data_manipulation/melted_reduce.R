cb8ff56a432a9bc12787b90ee3c99753d57cb8d6
HenriChap <henriquechapola@gmail.com>
2023-01-18 16:19:01 -0300

    Fix melted_reduce.R

--- resources/scripts_data_manipulation/melted_reduce.R
+++ resources/scripts_data_manipulation/melted_reduce.R
@@ -2,5 +2,5 @@ library(reshape)
 
 mock_samples <- read.delim("C:/Users/your_directory/Samples/mock_taxa_samples.txt")
 melted_mock<-melt(mock_samples)
-reduced_mock <- melted_mock[apply(melted_mock,1, function(x) all(x!= 0)),]
+reduced_mock <- melted_mock[melted_mock$value>0,]
 write.table(reduced_mock, file = "reduced_taxa_mock.txt", sep="\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
