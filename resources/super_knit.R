b0c490afaa8f45134018c1c2c38667d3f2683e03
HenriChap <henriquechapola@gmail.com>
2023-01-26 16:31:24 -0300

    Add  resources primary version

---
+++ resources/super_knit.R
@@ -0,0 +1,9 @@
+library(knitr)
+library(rmarkdown)
+
+#Com os dados reais, vai precisar converter o tipo de dado para list
+
+parameters <- list(list(taxa = "taxa1", name="Some namus"), list(taxa = "taxa2", name="Other namus"))
+for(i in 1:length(parameters)){
+  render("./template.Rmd", output_file = paste0("taxa", i, ".html"), params = parameters[[i]])
+}
