a1e0cc9e8645b3906fc6a2384604492a1dc1cb9e
HenriChap <henriquechapola@gmail.com>
2023-01-26 17:02:24 -0300

    Improve codes readability

--- resources/super_knit.R
+++ resources/super_knit.R
@@ -3,7 +3,7 @@ library(rmarkdown)
 
 #Com os dados reais, vai precisar converter o tipo de dado para list
 
-parameters <- list(list(taxa = "taxa1", name="Some namus"), list(taxa = "taxa2", name="Other namus"))
-for(i in 1:length(parameters)){
+parameters <- list(list(taxa = "taxa1", name = "Some namus"), list(taxa = "taxa2", name = "Other namus"))
+for (i in 1:length(parameters)) {
   render("./template.Rmd", output_file = paste0("taxa", i, ".html"), params = parameters[[i]])
 }
