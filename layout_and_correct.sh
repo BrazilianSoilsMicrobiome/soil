0601dda62962fac78e4b4e12ed419229e762692e
HenriChap <henriquechapola@gmail.com>
2023-02-23 17:17:19 -0300

    Update layout_and_correct.sh

--- layout_and_correct.sh
+++ layout_and_correct.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd $1
 # Iterate over all text files in the directory to add layout and correct Jekyll prone error
 for file in *.html; do
     #Add layout
@@ -8,5 +8,5 @@ for file in *.html; do
         sed -i '1d; 2 i ---\nlayout: default\n---' "$file"
     fi 
     # Perform the search and replace operation on each file
-    sed -i -r 's|\{\{(.*?)\}\}|{\1}|g' "$file"
+    perl -i -pe 's/\{\{(.*?)\}\}/\{\1\}/g' "$file"
 done
